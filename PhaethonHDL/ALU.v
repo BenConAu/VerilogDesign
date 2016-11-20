d58ecffeabadce6843044329a2ab7555ba73c671
Ben Constable <bencon@gmail.com>
2016-11-19 16:34:18 -0800

    Update verilog code for new ISA location

--- PhaethonHDL/ALU.v
+++ PhaethonHDL/ALU.v
@@ -20,7 +20,7 @@ module ALU(
   debug        // [Output] Debug port
   );
 
-  `include "../PhaethonAsm/CodeGen/PhaethonOpCode.v"
+  `include "../PhaethonISA/PhaethonOpCode.v"
 
   // Input / output
   input  wire        clk;
