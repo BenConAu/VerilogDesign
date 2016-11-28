771bb47ec214f523f5b1ddcad4d72f0f65065a23
Ben Constable <bencon@gmail.com>
2016-11-28 00:26:49 -0800

    Update baseline for FieldSelectWrite since it is passing

--- PhaethonSL/UnitTests/FieldSelectWrite.asm
+++ PhaethonSL/UnitTests/FieldSelectWrite.asm
@@ -3,6 +3,10 @@ mov r1, 1
 mov r0->foo::a, r1
 mov r1, 2
 mov r0->foo::b, r1
+mov r1, r0->foo::a
+dout r1
+mov r1, r0->foo::b
+dout r1
 stall
 TestFunction:
 mov r1, &fooVar
--- PhaethonSL/UnitTests/MultiplyLocal.asm
+++ PhaethonSL/UnitTests/MultiplyLocal.asm
@@ -4,9 +4,11 @@ mov r2, &fooVar
 fmul r3, r1
 mov r2->foo::a, r3
 mov r3, r2->foo::a
-fmul r4, r0
+fmul r4, r1
 mov r2->foo::b, r4
 mov r4, r2->foo::b
 fmul r3, r4
 mov r2->foo::c, r3
+mov r1, r2->foo::c
+dout r1
 stall
