d9b08e5bcbf9729e42c87c03d97f03809759090e
Ben Constable <bencon@gmail.com>
2016-12-08 00:39:14 -0800

    Expand test cases for multiple pointer field selection

--- PhaethonSL/UnitTests/DoubleFieldSelectRead.asm
+++ PhaethonSL/UnitTests/DoubleFieldSelectRead.asm
@@ -7,6 +7,14 @@ mov r1, r0->foo::next
 mov r2, 2
 mov r1->foo::value, r2
 mov r1, r0->foo::next
+mov r2, &yetAnother
+mov r1->foo::next, r2
+mov r1, r0->foo::next
+mov r2, r1->foo::next
+mov r1, 3
+mov r2->foo::value, r1
+mov r2, r0->foo::next
+mov r1, r2->foo::next
 mov r2, 0
 mov r1->foo::next, r2
 stall
