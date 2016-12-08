46b68581b847b335897cd3ec9d430e8bd8d0404c
Ben Constable <bencon@gmail.com>
2016-12-08 00:20:13 -0800

    Move AddressOfNode into cpp file

---
+++ PhaethonSL/AddressOfNode.cpp
@@ -0,0 +1,38 @@
+#include "AddressOfNode.h"
+#include "IdentifierNode.h"
+
+void AddressOfNode::VerifyNodeImpl()
+{
+    // We are getting the address of an identifier
+    IdentifierNode *pIdentifierNode = dynamic_cast<IdentifierNode *>(GetChild(0));
+    if (pIdentifierNode == nullptr)
+    {
+        throw "Need an identifier to take address";
+    }
+
+    // We are only allowed to get addresses of structs
+    VariableInfo *pVariableInfo = pIdentifierNode->GetVariableInfo();
+    StructTypeInfo *pStructInfo = dynamic_cast<StructTypeInfo *>(pVariableInfo->GetTypeInfo());
+    if (pStructInfo == nullptr)
+    {
+        throw "Can only take address of structs";
+    }
+}
+
+ExpressionResult *AddressOfNode::CalculateResult()
+{
+    // Find the result of the child
+    IdentifierNode *pIdentifierNode = dynamic_cast<IdentifierNode *>(GetChild(0));
+    std::unique_ptr<ExpressionResult> childResult(pIdentifierNode->CalculateResult());
+
+    // Since we know it is always a struct, we just need to return the new type with the same
+    // operand, since the operand was always loading up the address. Start by getting the
+    // child identifier node and the variable info out of it.
+    VariableInfo *pVariableInfo = pIdentifierNode->GetVariableInfo();
+
+    // Our expression type is pointer to the other expression type
+    TypeInfo *pNewTypeInfo = GetContext()->_typeCollection.GetPointerType(pVariableInfo->GetTypeInfo());
+
+    // The result has the new type but same path and same operand
+    return new ExpressionResult(pNewTypeInfo, childResult->_operand);
+}
--- PhaethonSL/AddressOfNode.h
+++ PhaethonSL/AddressOfNode.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "PSLCompilerContext.h"
-#include "ASTNode.h"
+#include "ExpressionNode.h"
 #include "PSL.tab.h"
 
 class AddressOfNode : public ExpressionNode
@@ -14,41 +14,7 @@ class AddressOfNode : public ExpressionNode
         AddNode(pExpr);
     }
 
-    void VerifyNodeImpl() override
-    {
-        // We are getting the address of an identifier
-        IdentifierNode *pIdentifierNode = dynamic_cast<IdentifierNode*>(GetChild(0));
-        if (pIdentifierNode == nullptr)
-        {
-            throw "Need an identifier to take address";
-        }
-
-        // We are only allowed to get addresses of structs
-        VariableInfo *pVariableInfo = pIdentifierNode->GetVariableInfo();
-        StructTypeInfo *pStructInfo = dynamic_cast<StructTypeInfo*>(pVariableInfo->GetTypeInfo());
-        if (pStructInfo == nullptr)
-        {
-            throw "Can only take address of structs";
-        }
-    }
-
+    void VerifyNodeImpl() override;
     bool IsConstant() const override { return true; }
-
-    ExpressionResult *CalculateResult() override
-    {
-        // Find the result of the child
-        IdentifierNode *pIdentifierNode = dynamic_cast<IdentifierNode*>(GetChild(0));
-        std::unique_ptr<ExpressionResult> childResult(pIdentifierNode->CalculateResult());
-
-        // Since we know it is always a struct, we just need to return the new type with the same
-        // operand, since the operand was always loading up the address. Start by getting the
-        // child identifier node and the variable info out of it.
-        VariableInfo *pVariableInfo = pIdentifierNode->GetVariableInfo();
-
-        // Our expression type is pointer to the other expression type
-        TypeInfo *pNewTypeInfo = GetContext()->_typeCollection.GetPointerType(pVariableInfo->GetTypeInfo());
-
-        // The result has the new type but same path and same operand
-        return new ExpressionResult(pNewTypeInfo, childResult->_operand);     
-    }
+    ExpressionResult *CalculateResult() override;
 };
