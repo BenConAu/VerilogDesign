d767d542a09ff4f2fad01f2974a75575f753c21f
Ben Constable <bencon@gmail.com>
2016-12-16 04:22:18 +0000

    Update generic function test case to have return value

--- PhaethonSL/UnitTests/DefineGenericFunction.asm
+++ PhaethonSL/UnitTests/DefineGenericFunction.asm
@@ -1,8 +1,13 @@
-mov r0, 0
-mov r1, @Func
-mov r4, r0
-rcall r1, 2
+mov r0, &TestVar
+mov r1, 1337
+mov r0->TestStruct::Member, r1
+mov r1, r0
+mov r3, @Func
+mov r6, r1
+rcall r3, 4
+mov r2, r6
+mov r6, r2->TestStruct::Member
+dout r6
 stall
 Func:
-dout r0
 rret
