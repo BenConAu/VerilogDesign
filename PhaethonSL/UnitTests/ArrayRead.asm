d7c48a5d008238755fdddaf6af66c8822067410d
Ben Constable <bencon@gmail.com>
2016-12-27 21:40:10 -0800

    Add more array test cases

--- PhaethonSL/UnitTests/ArrayRead.asm
+++ PhaethonSL/UnitTests/ArrayRead.asm
@@ -17,11 +17,25 @@ mov r2, r1
 mov r4, 0
 mov r1, 2
 mov r2[], r4, r1
-mov r2, r3->foo2::a
-dout r2
 add r2, r3, 4
 mov r4, r2
-mov r1, 0
-mov r2, r4[], r1
+mov r1, 4
+mov r2, 3
+mov r4[], r1, r2
+mov r4, 4
+mov r3->foo2::c, r4
+mov r4, r3->foo2::a
+dout r4
+add r4, r3, 4
+mov r1, r4
+mov r2, 0
+mov r4, r1[], r2
+dout r4
+add r1, r3, 4
+mov r2, r1
+mov r4, 4
+mov r1, r2[], r4
+dout r1
+mov r2, r3->foo2::c
 dout r2
 stall
