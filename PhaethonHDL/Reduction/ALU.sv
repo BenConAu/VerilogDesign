c247a16f088d3a9f3d78ec7fdf2a656f6c281082
Ben Constable <bencon@gmail.com>
2017-01-03 18:42:59 -0800

    Reducing ALU

--- PhaethonHDL/Reduction/ALU.sv
+++ PhaethonHDL/Reduction/ALU.sv
@@ -20,8 +20,6 @@ module ALU(
   debug        // [Output] Debug port
   );
 
-  `include "../PhaethonISA/Generated/PhaethonOpCode.v"
-
   // Input / output
   input  wire        clk;
   input  wire        reset;
@@ -66,17 +64,6 @@ module ALU(
   wire       [31:0] fDivResult;
   wire       [31:0] floatDebug;
   wire       [1:0]  fCompareResult;
-
-  FloatingAdd         fAdd0(regValue2[0], regValue3[0], 1'b0, fAddResult[0], floatDebug, clk, fOpEnable[0:0]);
-  FloatingAdd         fAdd1(regValue2[1], regValue3[1], 1'b0, fAddResult[1], floatDebug, clk, fOpEnable[0:0]);
-  FloatingAdd         fAdd2(regValue2[2], regValue3[2], 1'b0, fAddResult[2], floatDebug, clk, fOpEnable[0:0]);
-  FloatingAdd         fAdd3(regValue2[3], regValue3[3], 1'b0, fAddResult[3], floatDebug, clk, fOpEnable[0:0]);
-  FloatingAdd         fSub(regValue[0], regValue2[0], 1'b1, fSubResult, floatDebug, clk, fOpEnable[1:1]);
-  FloatingFromInt     fConv(regValue[0], fConvResult, floatDebug, clk, fOpEnable[2:2]);
-  FloatingMultiply    fMul(regValue2[0], regValue3[0], fMulResult, floatDebug, clk, fOpEnable[3:3]);
-  FloatingMultiplyAdd fMulAdd(regValue[0], regValue2[0], regValue3[0], fMulAddResult, floatDebug, clk, fOpEnable[4:4]);
-  FloatingCompare     fComp(regValue[0], regValue2[0], fCompareResult, floatDebug, clk, fOpEnable[5:5]);
-  FloatingDivide      fDiv(regValue[0], regValue2[0], fDivResult, floatDebug, clk, fOpEnable[6:6]);
 
   //initial
   //   $monitor("%t, ram = %h, %h, %h, %h : %h, %h, %h, %h",
@@ -146,412 +133,32 @@ module ALU(
       2: begin
         // Read values from registers
         regValue[0] <= regarray[regAddress[7:0]];
-        if (opCode == `VfaddRRR)
-        begin
-          regValue[1] <= regarray[regAddress[7:0] + 1];
-          regValue[2] <= regarray[regAddress[7:0] + 2];
-          regValue[3] <= regarray[regAddress[7:0] + 3];
-        end
-
         regValue2[0] <= regarray[regAddress2[7:0]];
-        if (opCode == `VfaddRRR)
-        begin
-          regValue2[1] <= regarray[regAddress2[7:0] + 1];
-          regValue2[2] <= regarray[regAddress2[7:0] + 2];
-          regValue2[3] <= regarray[regAddress2[7:0] + 3];
-        end
-
         regValue3[0] <= regarray[regAddress3[7:0]];
-        if (opCode == `VfaddRRR)
-        begin
-          regValue3[1] <= regarray[regAddress3[7:0] + 1];
-          regValue3[2] <= regarray[regAddress3[7:0] + 2];
-          regValue3[3] <= regarray[regAddress3[7:0] + 3];
-        end
 
-        // Enable operation for module
-        if (opCode == `FaddRRR || opCode == `VfaddRRR) fOpEnable[0:0] <= 1;
-        if (opCode == `FsubRR) fOpEnable[1:1] <= 1;
-        if (opCode == `FconvR) fOpEnable[2:2] <= 1;
-        if (opCode == `FmulRRR) fOpEnable[3:3] <= 1;
-        if (opCode == `FmuladdRRR) fOpEnable[4:4] <= 1;
-        if (opCode == `FminRR) fOpEnable[5:5] <= 1;
-        if (opCode == `FmaxRR) fOpEnable[5:5] <= 1;
-        if (opCode == `FdivRR) fOpEnable[6:6] <= 1;
-
-        // Determine if a conditional jump needs to happen
-        if (opCode == `JneC && regarray[1][0:0] == 1'b0) condJump <= 1'b1;
-        if (opCode == `JeC && regarray[1][0:0] == 1'b1) condJump <= 1'b1;
-        if (opCode == `JzRC && regarray[regAddress3[7:0]] == 0) condJump <= 1'b1;
-        if (opCode == `JnzRC && regarray[regAddress3[7:0]] != 0) condJump <= 1'b1;
-
-        if (Is8ByteOpcode(opCode) == 1)
-        begin
-          // Read values from ram requested by instruction
-          readReq <= 1;
-          ramAddress <= ipointer + 4;
-
-          // We need to move into further modes
-          mode <= 3;
-        end
-        else if (IsRAMOpcode(opCode) == 1)
-        begin
-          // We are not reading a constant, but we might still be
-          // doing a RAM operation, ove into the mode where we do that
-          mode <= 4;
-        end
-        else
-        begin
-          // Since there is no word data, there is no need
-          // to wait for that word data to come back, and there
-          // can be no further reads or writes since the word
-          // data is where the address would go
-          mode <= 6;
-        end
+        // Since there is no word data, there is no need
+        // to wait for that word data to come back, and there
+        // can be no further reads or writes since the word
+        // data is where the address would go
+        mode <= 6;
 
         debug[23:0] <= regAddress;
         debug[31:24] <= mode;
-      end
-
-      // Mode 3: Complete read of additional code for opCodes that
-      //         store word data. We only end up in this mode if
-      //         the appropriate opCode is set, so no need to check
-      //         the opCode.
-      3: begin
-        // Stop request
-        readReq <= 0;
-
-        if (readAck == 1)
-        begin
-          // Store ram values requested
-          opDataWord <= ramIn;
-
-          // Move to next mode - only progress to data read / write
-          // for opCodes that actually need it.
-          if (IsRAMOpcode(opCode) == 1)
-            mode <= 4;
-          else
-            mode <= 6;
-        end
-
-        debug[23:0] <= ramIn;
-        debug[31:24] <= mode;
-      end
-
-      // Mode 4: Initiate data read or write if the instruction
-      //         requires it.
-      4: begin
-        if (opCode == `MovRdC)
-        begin
-          // Read values from address encoded in code
-          readReq <= 1;
-          ramAddress <= opDataWord;
-
-          //$display("Requesting RdC read from %h", opDataWord);
-        end
-
-        if (opCode == `MovRdRo)
-        begin
-          // First register is destination, second register is base address, 
-          // constant stores offset in bytes.
-          readReq <= 1;
-          ramAddress <= opDataWord + regValue2[0];
-
-          //$display("Requesting read from %h", opDataWord + regValue2);
-        end
-
-        if (opCode == `MovRdRoR)
-        begin
-          // First register is destination, second register is base address, 
-          // constant stores size of item, and third register stores index of item.
-          readReq <= 1;
-          ramAddress <= regValue2[0] + opDataWord * regValue3[0];
-
-          //$display("Requesting read from %h", regValue2[0] + opDataWord * regValue3[0]);
-        end
-
-        if (opCode == `MovRdR)
-        begin
-          // Read values from address encoded in code
-          readReq <= 1;
-          ramAddress <= regValue2[0];
-
-          //$display("Requesting read from %h", opDataWord + regValue2);
-        end
-
-        if (opCode == `PopR || opCode == `Ret)
-        begin
-          // Read value from stack
-          readReq <= 1;
-          ramAddress <= regarray[0] - 4;
-
-          //$display("Requesting read from %h - 4", regarray[0]);
-        end
-
-        if (opCode == `MovdCR)
-        begin
-          // Write values to ram requested by instruction
-          writeReq <= 1;
-          ramAddress <= opDataWord;
-          ramOut <= regValue2[0];
-
-          //$display("Reqesting write %h to address value %h", regValue2[0], opDataWord);
-        end
-
-        if (opCode == `MovdRoR)
-        begin
-          // Write values to ram requested by instruction
-          writeReq <= 1;
-          ramAddress <= opDataWord + regValue[0];
-          ramOut <= regValue2[0];
-
-          //$display("Reqesting write %h to address value %h", regValue2[0], opDataWord + regValue[0]);
-        end
-
-        if (opCode == `MovdRoRR)
-        begin
-          // first register is base address, constant stores size of items, 
-          // second register stores index of item, third register is destination, 
-          writeReq <= 1;
-          ramAddress <= regValue[0] + opDataWord * regValue2[0];
-          ramOut <= regValue3[0];
-
-          //$display("Reqesting write %h to address value %h", regValue2[0], regValue[0] + opDataWord * regValue2[0]);
-        end
-
-        if (opCode == `PushR)
-        begin
-          // Write register value to stack
-          writeReq <= 1;
-          ramAddress <= regarray[0];
-          ramOut <= regValue[0];
-
-          //$display("Reqesting push %h", regValue);
-        end
-
-        if (opCode == `CallR)
-        begin
-          // Write ip to stack
-          writeReq <= 1;
-          ramAddress <= regarray[0];
-          ramOut <= ipointer;
-
-          //$display("Reqesting push %h", regValue);
-        end
-
-        debug[23:0] <= opDataWord;
-        debug[31:24] <= mode;
-
-        mode <= 5;
-      end
-
-      // Mode 5: Complete data read or write if the instruction
-      //         requires it.
-      5: begin
-        if (opCode == `MovRdC || opCode == `MovRdRo || opCode == `MovRdRoR || opCode == `MovRdR || opCode == `PopR || opCode == `Ret)
-        begin
-          // Stop request
-          readReq <= 0;
-
-          if (readAck)
-          begin
-            //$display("Receiving read address value %h", ramIn);
-
-            // Store ram values requested
-            ramValue <= ramIn;
-
-            // Now we can move on
-            mode <= 6;
-          end
-        end
-        else if (opCode == `MovdCR || opCode == `MovdRoR || opCode == `MovdRoRR || opCode == `PushR || opCode == `CallR)
-        begin
-          // Stop request
-          writeReq <= 0;
-
-          if (writeAck == 1)
-          begin
-            // Can move to next mode
-            mode <= 6;
-          end
-        end
-        else
-          mode <= 6;
-
-        debug[23:0] <= ramIn[23:0];
-        debug[31:24] <= mode;
-
       end
 
       // Mode 6: Finalize the instruction operation, by performing
       //         the writes that are needed and moving the
       //         instruction pointer along.
       6: begin
-        // Now we can do writes to non-ram things
-        case (opCode)
-          `MovRC:    regarray[regAddress[7:0]] <= opDataWord;             // mov reg, const
-          `MovRdC:   regarray[regAddress[7:0]] <= ramValue;               // mov reg, [addr]
-          `MovRR:    regarray[regAddress[7:0]] <= regValue2[0];           // mov reg, reg
-          `MovRdRo:  regarray[regAddress[7:0]] <= ramValue;               // mov reg, [reg + const]
-          `MovRdRoR: regarray[regAddress[7:0]] <= ramValue;               // mov reg, [reg + const * reg]
-          `MovRdR:   regarray[regAddress[7:0]] <= ramValue;               // mov reg, [reg]
-          `MovdCR:   begin end // Done above
-          `MovdRoR:  begin end // Done above
-          `MovdRoRR: begin end // Done above
-
-          `PushR: begin
-            regarray[0] <= regarray[0] + 4;                             // push reg
-          end
-
-          `PopR: begin
-            regarray[regAddress[7:0]] <= ramValue;                        // pop reg
-            regarray[0] <= regarray[0] - 4;
-          end
-
-          `CallR: begin
-            // Stack pointer increases like a push
-            regarray[0] <= regarray[0] + 4;                             // call reg
-
-            // Jump to the function
-            ipointer <= regValue[0];
-
-            //$display("Doing a call");
-          end
-
-          `Ret: begin
-            // Stack pointer decreases like a pop
-            regarray[0] <= regarray[0] - 4;                             // ret
-
-            // Return to the saved position, after the call
-            ipointer <= ramValue + 4;
-
-            //$display("Doing a ret");
-          end
-
-          `RCallRC: begin
-            // Store return address
-            regarray[rPos + opDataWord] <= ipointer;
-
-            // Store arg value
-            regarray[rPos + opDataWord + 1] <= opDataWord;
-
-            // Shift register window
-            rPos <= rPos + opDataWord + 2;
-
-            // Jump to the function
-            ipointer <= regValue[0];
-
-            //$display("Doing a register shift call to %h", regValue[0]);
-          end
-
-          `RRet: begin
-            // Get the return address
-            ipointer <= regarray[rPos - 2] + 8;
-
-            // Get the window value
-            rPos <= rPos - (regarray[rPos - 1] + 2);
-
-            //$display("Doing an rret");
-          end
-
-          `CmpRR: begin                                                   // cmp reg, reg
-            regarray[1][0:0] <= (regValue[0] == regValue2[0] ? 1 : 0);
-            regarray[1][1:1] <= (regValue[0] < regValue2[0] ? 1 : 0);
-            regarray[1][2:2] <= (regValue[0] > regValue2[0] ? 1 : 0);
-          end
-
-          `CmpRC: begin
-            regarray[1][0:0] <= (regValue[0] == opDataWord ? 1 : 0);
-            regarray[1][1:1] <= (regValue[0] < opDataWord ? 1 : 0);
-            regarray[1][2:2] <= (regValue[0] > opDataWord ? 1 : 0);
-          end
-
-          `CmpERRR: begin                                                 // cmpe reg, reg, reg
-            regarray[regAddress[7:0]] <= (regValue2[0] == regValue3[0] ? 1 : 0);
-          end
-
-          `CmpNeRRR: begin                                                 // cmpne reg, reg, reg
-            regarray[regAddress[7:0]] <= (regValue2[0] != regValue3[0] ? 1 : 0);
-          end
-
-          `CmpLtRRR: begin                                                 // cmplt reg, reg, reg
-            regarray[regAddress[7:0]] <= (regValue2[0] < regValue3[0] ? 1 : 0);
-          end
-
-          `CmpGtRRR: begin                                                 // cmpgt reg, reg, reg
-            regarray[regAddress[7:0]] <= (regValue2[0] > regValue3[0] ? 1 : 0);
-          end
-
-          `JmpC:  ipointer <= opDataWord;                              // jmp address
-
-          `JneC: begin end // Done above
-          `JeC: begin end   // Done above
-          `JzRC: begin end // Done above
-          `JnzRC: begin end   // Done above
-
-          `FaddRRR:    regarray[regAddress[7:0]] <= fAddResult[0];          // fadd reg, reg, reg
-          `FsubRR:     regarray[regAddress[7:0]] <= fSubResult;             // fsub reg, reg
-          `FconvR:     regarray[regAddress[7:0]] <= fConvResult;            // fconv reg
-          `FmulRRR:    regarray[regAddress[7:0]] <= fMulResult;             // fmul reg, reg, reg
-          `FdivRR:     regarray[regAddress[7:0]] <= fDivResult;
-          `FmuladdRRR: regarray[regAddress[7:0]] <= fMulAddResult;          // fmul reg, reg
-          `FminRR:     regarray[regAddress[7:0]] <= (fCompareResult == 'b01 ? regValue2[0] : regValue[0]);
-          `FmaxRR:     regarray[regAddress[7:0]] <= (fCompareResult == 'b11 ? regValue2[0] : regValue[0]);
-
-          `VfaddRRR: begin
-            regarray[regAddress[7:0]] <= fAddResult[0];
-            regarray[regAddress[7:0] + 1] <= fAddResult[1];
-            regarray[regAddress[7:0] + 2] <= fAddResult[2];
-            regarray[regAddress[7:0] + 3] <= fAddResult[3];
-          end
-
-          `AddRRC:     regarray[regAddress[7:0]] <= regValue2[0] + opDataWord;   // add reg, reg, const
-          `AddRRR:     regarray[regAddress[7:0]] <= regValue2[0] + regValue3[0]; // add reg, reg, reg
-          `SubRRC:     regarray[regAddress[7:0]] <= regValue2[0] - opDataWord;   // add reg, reg, const
-          `SubRRR:     regarray[regAddress[7:0]] <= regValue2[0] - regValue3[0]; // add reg, reg, reg
-          `IncR:       regarray[regAddress[7:0]] <= regValue[0] + 1;             // dec reg
-          `DecR:       regarray[regAddress[7:0]] <= regValue[0] - 1;             // dec reg
-          `ShlRRR:     regarray[regAddress[7:0]] <= regValue2[0] << regValue3[0];// shl reg, reg, reg
-          `ShrRRR:     regarray[regAddress[7:0]] <= regValue2[0] >> regValue3[0];// shl reg, reg, reg
-          `NegRR:      regarray[regAddress[7:0]] <= -regValue2[0];               // neg reg, reg
-
-          `MulAddRRC:  regarray[regAddress[7:0]] <= regValue[0] + regValue2[0] * opDataWord;
-
-          `DoutR: begin
-            $display("DebugOut %h", regValue[0]);
-          end
-
-          `Stall: begin 
-          end
-
-          default: $display("Unknown instruction %h", opCode);
-        endcase
 
         //$display("opDataWord == %h", opDataWord);
 
         debug[23:0] <= regAddress;
         debug[31:24] <= mode;
 
-        // Move the instruction pointer along
-        if (condJump == 1'b1)
-        begin
-          ipointer <= opDataWord;
-        end
-        else if (opCode != `JmpC &&
-            opCode != `CallR &&
-            opCode != `Ret &&
-            opCode != `RCallRC &&
-            opCode != `RRet &&
-            opCode != `Stall
-            )
-        begin
-          //$display("Incrementing ip");
+        //$display("Incrementing ip");
 
-          if (Is8ByteOpcode(opCode) == 1)
-            ipointer <= ipointer + 8;
-          else
-            ipointer <= ipointer + 4;
-        end
+        ipointer <= ipointer + 4;
 
         //$display("Finished instruction %h", opCode);
 
---
+++ PhaethonHDL/Reduction/ALU_tb.sv
@@ -0,0 +1,124 @@
+/* `timescale 10us/10ns */
+
+module test;
+
+  /* Make a reset that pulses once. */
+  reg reset = 0;
+  initial begin
+    # 0 reset = 1;
+    # 1 reset = 0;
+    # 20000 $finish;
+  end
+
+  reg [7:0] fileRam[0:2047];
+  reg [511:0] testname;
+  reg fileFound;
+
+  initial
+  begin
+    fileFound = $value$plusargs("ROMFILE=%s", testname);
+    $readmemh(testname, fileRam, 0);
+  end
+
+  /* Make a regular pulsing clock. */
+  reg clk = 0;
+  always #5 clk = !clk;
+  reg [31:0] ramValue;
+
+  wire [31:0] iPointer;
+  wire [7:0] opCode;
+  wire [31:0] r0;
+  wire [31:0] r1;
+  wire [31:0] r2;
+  wire [31:0] r3;
+  wire [31:0] r4;
+  wire [31:0] r5;
+  wire [7:0] rPos;
+  wire [31:0] debug;
+  wire [31:0] ramAddress;
+  wire [31:0] ramOut;
+  wire [0:0] readReq;
+  wire [0:0] writeReq;
+
+  reg [31:0] reqAddress;
+  reg [0:0] readAck;
+  reg [0:0] writeAck;
+  reg [7:0] mode = 0;
+
+  parameter RAMSIZE = 64;
+
+  ALU alu(
+    clk,
+    reset,
+    ramValue,
+    readAck,
+    writeAck,
+    ramAddress,
+    ramOut,
+    readReq,
+    writeReq,
+    iPointer,
+    opCode,
+    r0,
+    r1,
+    r2,
+    r3,
+    r4,
+    r5,
+    rPos,
+    debug
+    );
+
+  //initial
+    // $monitor("At time %t, ip = %h, opCode = %h, reset = %h, r[0:1:2:3:4:5] = %h:%h:%h:%h:%h:%h, rPos = %h, debug = %h",
+      //        $time, iPointer[15:0], opCode, reset, r0, r1, r2, r3, r4, r5, rPos, debug);
+
+  always @(posedge clk)
+  begin
+    // Mode zero means ready to fill a request (so that you can't file
+    // a new request in the middle of another one)
+    if (mode == 0)
+    begin
+      // Ack bits are only set when read or write is done
+      readAck <= 0;
+      writeAck <= 0;
+
+      if (readReq == 1)
+      begin
+        reqAddress <= ramAddress;
+        mode <= 1;
+      end
+
+      if (writeReq == 1)
+      begin
+        reqAddress <= ramAddress;
+        mode <= 2;
+      end
+    end
+
+    // Mode one means the request from the outside is understood and
+    // the read will be processed and the acknowledgement sent back
+    if (mode == 1)
+    begin
+      //$display("Retrieving address %h", reqAddress);
+      ramValue[7:0] <= fileRam[reqAddress];
+      ramValue[15:8] <= fileRam[reqAddress + 1];
+      ramValue[23:16] <= fileRam[reqAddress + 2];
+      ramValue[31:24] <= fileRam[reqAddress + 3];
+      readAck <= 1;
+      mode <= 0;
+    end
+
+    if (mode == 2)
+    begin
+      //$display("Writing %h into %h", ramOut, reqAddress);
+      fileRam[reqAddress] <= ramOut[7:0];
+      fileRam[reqAddress + 1] <= ramOut[15:8];
+      fileRam[reqAddress + 2] <= ramOut[23:16];
+      fileRam[reqAddress + 3] <= ramOut[31:24];
+      writeAck <= 1;
+      mode <= 0;
+    end
+  end
+
+endmodule // test
