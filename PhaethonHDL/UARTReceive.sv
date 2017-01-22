bb0b303003819cb4e493c5e6ca8c7ab72b7f70cb
BenConAu <bencon@gmail.com>
2017-01-21 23:57:11 -0800

    Working 115200 baud UART

--- PhaethonHDL/UARTReceive.sv
+++ PhaethonHDL/UARTReceive.sv
@@ -3,19 +3,20 @@ module UARTReceive(
   reset,
   rxd,
   dataComplete,
-  zeroBits,
-  oneBits
+  dataOutput,
+  startCount
   );
 
   output reg dataComplete;
-  output reg[31:0] zeroBits;
-  output reg[31:0] oneBits;
+  output reg[7:0] dataOutput;
+  output reg[31:0] startCount;
   input wire clk;
   input wire reset;
   input wire rxd;
 
-  reg [7:0] state;
-  reg [7:0] counter;
+  reg [15:0] state;
+  reg [15:0] counter;
+  reg [7:0] data;
 
   always @(posedge clk or posedge reset)
   begin
@@ -23,76 +24,62 @@ module UARTReceive(
     begin
       state <= 0;
       dataComplete <= 0;
-      //data <= 0;
+      data <= 0;
       counter <= 0;
     end
     else
     begin
-/*      if (state == 0)
-      begin
-        if (counter < 217)
-        begin
-          if (rxd == 1)
-          begin
-            // Did not maintain start state for long enough
-            counter <= 0;
-          end
-          else
-          begin
-            // Keep counting
-            counter <= counter + 1;
-          end
-        end
-        else
-        begin
-          // Look for first bit
-          state <= 1;
-          counter <= 0;
-        end
-      end
-      else if (state == 9)
+      if (state == 0)
       begin
         if (rxd == 1)
         begin
-          // Valid stop bit
-          dataComplete <= 1;
-        end
-
-        state <= 0;
-      end
-      else
-      begin
-        // Look for next bit
-        if (counter < 434)
-        begin
-          counter <= counter + 1;
+          // Did not maintain start state for long enough    
+          counter <= 0;
         end
         else
         begin
-          counter <= 0;
-          data[state - 1] <= rxd;
-          state <= state + 1;
+          if (counter == 217)
+          begin
+            // We have decided this is a start bit
+            startCount <= startCount + 1;
+            counter <= 0;
+            data <= 0;
+    
+            // Start grabbing data bits
+            state <= 1;
+          end
+          else
+          begin
+            counter <= counter + 1;
+          end
         end
       end
-    end*/
-	 
-	   if (rxd == 1)
-	   begin
-		  if (state == 0)
-		  begin
-  		    state <= 1;
-	       oneBits <= oneBits + 1;
-		  end
-	   end
-		else
-		begin
-		  if (state == 1)
-		  begin
-   	    state <= 0;
-	       zeroBits <= zeroBits + 1;
-		  end
-		end
-	 end
+      else
+      begin
+        if (counter == 434)
+        begin
+          if (state == 9)
+          begin
+            // This was the stop bit, go back to start state
+            state <= 0;
+            dataOutput <= data;
+          end
+          else
+          begin
+            // Add the bit to our data register and look for the next one
+            state <= state + 1;
+            data[state - 1] <= rxd;   
+          end
+    
+          // No matter what the counter resets
+          counter <= 0;
+        end
+        else
+        begin
+          counter <= counter + 1;
+        end
+      end
+    end
   end
   
 endmodule
