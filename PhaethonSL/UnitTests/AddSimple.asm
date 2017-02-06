6d05b0b5c467694708153dd02f4bb2d3333a2e2f
Ben Constable <bencon@gmail.com>
2017-02-05 23:24:31 -0800

    Add hex syntax for ints

---
+++ PhaethonSL/UnitTests/AddSimple.asm
@@ -0,0 +1,6 @@
+mov r0, 2748
+mov r1, 291
+add r3, r0, r1
+mov r2, r3
+dout r2
+stall
--- PhaethonSL/lex.h
+++ PhaethonSL/lex.h
@@ -336,7 +336,7 @@ extern int yylex \
 #undef YY_DECL
 #endif
 
-#line 74 "PSL.l"
+#line 75 "PSL.l"
 
 
 #line 343 "lex.h"
