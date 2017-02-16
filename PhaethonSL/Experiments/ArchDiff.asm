a776b8b51d951938d1bf359e911fa0fa8fd10181
Ben Constable <bencon@gmail.com>
2017-02-16 00:03:07 -0800

    Reduce...

--- PhaethonSL/Experiments/ArchDiff.asm
+++ PhaethonSL/Experiments/ArchDiff.asm
@@ -4,14 +4,9 @@ mov r2, r1
 mov r3, 0
 mov r1, 37
 mov [r2:sizeof(Word)], r3, r1
-add r3, r0, 0
-mov r2, r3
+add r2, r0, 0
+mov r3, r2
 mov r1, 0
-mov r3, [r2:sizeof(Word)], r1
-dout r3
-mov r1, &foo2Var
-mov r2, 1
-mov r1->foo2::a, r2
-mov r2, r1->foo2::a
+mov r2, [r3:sizeof(Word)], r1
 dout r2
 stall
