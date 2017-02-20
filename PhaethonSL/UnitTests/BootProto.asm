378dd7858ae7aa76ecedf100e96be6ab6ff6979a
Ben Constable <bencon@gmail.com>
2017-02-19 19:15:15 -0800

    Fix firmware prototypes

--- PhaethonSL/UnitTests/BootProto.asm
+++ PhaethonSL/UnitTests/BootProto.asm
@@ -17,7 +17,21 @@ mov r7, 4025417775
 mov [r5:sizeof(Word)], r6, r7
 mov r5, r4
 mov r6, 3
-mov r7, 4025478964
+mov r7, 4025478966
 mov [r5:sizeof(Word)], r6, r7
+mov r5, 1
+dout r5
+push r0
+push r1
+push r2
+push r3
+push r4
 exec r4
+pop r4
+pop r3
+pop r2
+pop r1
+pop r0
+mov r5, 2
+dout r5
 stall
