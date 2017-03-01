76ecb93c01ae44dba2bcde2d2e42c86fc9667aef
Ben Constable <bencon@gmail.com>
2017-02-28 22:20:29 -0800

    Refactor memory + alu into cpu

--- PhaethonHDL/ALU_tb.sv
+++ PhaethonHDL/ALU_tb.sv
@@ -10,6 +10,10 @@ module test;
     # 20000 $finish;
   end
 
+  /* Make a regular pulsing clock. */
+  reg clk = 0;
+  always #5 clk = !clk;
+
   reg [7:0] fileRam[0:2047];
   reg [511:0] testname;
   reg fileFound;
@@ -19,97 +23,6 @@ module test;
     fileFound = $value$plusargs("ROMFILE=%s", testname);
     $readmemh(testname, fileRam, 0);
   end
-
-  /* Make a regular pulsing clock. */
-  reg clk = 0;
-  always #5 clk = !clk;
-
-  wire [31:0] iPointer;
-  wire [7:0] opCode;
-  wire [31:0] r0;
-  wire [31:0] r1;
-  wire [31:0] r2;
-  wire [31:0] r3;
-  wire [31:0] r4;
-  wire [31:0] r5;
-  wire [7:0] rPos;
-  wire [31:0] debug;
-  wire [31:0] debug2;
-  wire [8:0] debug3;
-  wire [31:0] mcRamAddress;
-  wire [31:0] mcRamRead;
-  wire [31:0] mcRamWrite;
-  wire [0:0] mcReadReq;
-  wire [0:0] mcWriteReq;
-  wire [0:0] uartReadReq;
-  reg [31:0] phRamRead;
-  wire [31:0] phRamWrite;
-  wire [31:0] phRamAddress;
-  wire [0:0] phReadReq;
-  wire [0:0] phWriteReq;
-
-  reg  [7:0] uartData = 'hab;
-  reg  [0:0] uartReadAck = 1'b0;
-
-  wire uartWriteReq;
-  wire [7:0] uartWriteData;
-  reg uartWriteReady = 1'b1;
-
-  wire mcAddrVirtual = 0;
-
-  ALU alu(
-    clk,
-    reset,
-    mcRamRead,
-    mcRamReady,
-    mcRamAddress,
-    mcRamWrite,
-    mcReadReq,
-    mcWriteReq,
-    mcAddrVirtual,
-    uartReadReq,    // [Output] uart read requested
-    uartReadAck,    // [Input]  Flag to indicate read success
-    uartData,       // [Input] Actual data read 
-    uartWriteReq,   // [Output] uart write requested
-    uartWriteData,  // [Output] uart data to write
-    uartWriteReady, // [Input]  uart ready to send
-    iPointer,
-    opCode,
-    r0,
-    r1,
-    r2,
-    r3,
-    r4,
-    r5,
-    rPos,
-    debug,
-    debug2,
-    debug3
-    );
-
-  //initial
-     //$monitor("At time %t, ip = %h, opCode = %h, readReq = %h, mcRamReady = %h, ramValue = %h, uartRR = %h, uartRA = %h, uartD = %h, r[0:1:2:3:4:5] = %h:%h:%h:%h:%h:%h, rPos = %h, debug = %h",
-        //$time, iPointer[15:0], opCode, mcReadReq, mcRamReady, mcRamRead, uartReadReq, uartReadAck, uartData, r0, r1, r2, r3, r4, r5, rPos, debug);
-
-  wire [31:0] mcDebug;
-
-  MemoryController mc1(
-    clk,              // Global clock
-    reset,            // Reset
-    mcRamRead,        // [Output] RAM at requested address
-    mcRamReady,       // [Output] RAM is ready to be picked up
-    mcRamAddress,     // [Input] RAM address requested
-    mcRamWrite,       // [Input] RAM to write
-    mcReadReq,        // [Input] RAM read request
-    mcWriteReq,       // [Input] RAM write request
-    mcAddrVirtual,    // [Input] Virtual flag for RAM
-    phRamRead,        // [Input]  RAM at requested address
-    phRamAddress,     // [Output] RAM address requested
-    phRamWrite,       // [Output] RAM to write
-    phReadReq,        // [Output] RAM read request
-    phWriteReq,       // [Output] RAM write request
-    mcDebug           // [Output] Debug port
-  );
 
   // Fake RAM
   always @(posedge clk)
@@ -133,6 +46,9 @@ module test;
     end
   end
 
+  reg  [7:0] uartReadData = 'hab;
+  reg  [0:0] uartReadAck = 1'b0;
+
   // Fake UART receiver
   always @(posedge clk)
   begin
@@ -142,6 +58,10 @@ module test;
     end
   end  
 
+  wire       uartWriteReq;
+  wire [7:0] uartWriteData;
+  reg        uartWriteReady = 1'b1;
+
   // Fake UART sender
   always @(posedge clk)
   begin
@@ -150,5 +70,28 @@ module test;
       $display("UART send with data %h", uartWriteData);
     end
   end  
+
+  wire [0:0]  uartReadReq;
+  reg  [31:0] phRamRead;
+  wire [31:0] phRamWrite;
+  wire [31:0] phRamAddress;
+  wire [0:0]  phReadReq;
+  wire [0:0]  phWriteReq;
+
+  CPU cpu1(
+    clk,            // [Input]  Clock driving the ALU
+    reset,          // [Input]  Reset pin
+    phRamRead,      // [Input]  RAM at requested address
+    phRamAddress,   // [Output] RAM address requested
+    phRamWrite,     // [Output] RAM to write
+    phReadReq,      // [Output] RAM read request
+    phWriteReq,     // [Output] RAM write request  
+    uartReadReq,    // [Output] uart read requested
+    uartReadAck,    // [Input]  Flag to indicate read success
+    uartReadData,   // [Input]  Actual data read 
+    uartWriteReq,   // [Output] uart write requested
+    uartWriteData,  // [Output] uart data to write
+    uartWriteReady, // [Input]  uart ready to send
+    );
 
 endmodule // test
---
+++ PhaethonHDL/CPU.sv
@@ -0,0 +1,105 @@
+module CPU(
+  clk,            // [Input]  Clock driving the ALU
+  reset,          // [Input]  Reset pin
+  phRamRead,      // [Input]  RAM at requested address
+  phRamAddress,   // [Output] RAM address requested
+  phRamWrite,     // [Output] RAM to write
+  phReadReq,      // [Output] RAM read request
+  phWriteReq,     // [Output] RAM write request  
+  uartReadReq,    // [Output] uart read requested
+  uartReadAck,    // [Input]  Flag to indicate read success
+  uartReadData,   // [Input]  Actual data read 
+  uartWriteReq,   // [Output] uart write requested
+  uartWriteData,  // [Output] uart data to write
+  uartWriteReady, // [Input]  uart ready to send
+  );
+
+  input wire [0:0]   clk;
+  input wire [0:0]   reset;
+  input wire [31:0]  phRamRead;
+  output wire [31:0] phRamAddress;
+  output wire [31:0] phRamWrite;
+  output wire [0:0]  phReadReq;  
+  output wire [0:0]  phWriteReq;
+  output wire [0:0]  uartReadReq;
+  input wire [0:0]   uartReadAck;
+  input wire [7:0]   uartReadData;
+  output wire        uartWriteReq;
+  output wire [7:0]  uartWriteData;
+  input wire         uartWriteReady;
+
+  wire [31:0]  mcRamRead;
+  wire [31:0]  mcRamAddress;
+  wire [31:0]  mcRamWrite;
+  wire [0:0]   mcReadReq;
+  wire [0:0]   mcWriteReq;
+  wire [0:0]   mcAddrVirtual;
+  
+  wire [31:0]  iPointer;
+  wire [7:0]   opCode;
+  wire [31:0]  r0;
+  wire [31:0]  r1;
+  wire [31:0]  r2;
+  wire [31:0]  r3;
+  wire [31:0]  r4;
+  wire [31:0]  r5;
+  wire [7:0]   rPos;
+  wire [31:0]  debug;
+  wire [31:0]  debug2;
+  wire [8:0]   debug3;
+
+  ALU alu(
+    clk,
+    reset,
+    mcRamRead,
+    mcRamReady,
+    mcRamAddress,
+    mcRamWrite,
+    mcReadReq,
+    mcWriteReq,
+    mcAddrVirtual,
+    uartReadReq,    // [Output] uart read requested
+    uartReadAck,    // [Input]  Flag to indicate read success
+    uartReadData,   // [Input] Actual data read 
+    uartWriteReq,   // [Output] uart write requested
+    uartWriteData,  // [Output] uart data to write
+    uartWriteReady, // [Input]  uart ready to send
+    iPointer,
+    opCode,
+    r0,
+    r1,
+    r2,
+    r3,
+    r4,
+    r5,
+    rPos,
+    debug,
+    debug2,
+    debug3
+    );
+
+  wire [31:0] mcDebug;
+
+  MemoryController mc1(
+    clk,              // Global clock
+    reset,            // Reset
+    mcRamRead,        // [Output] RAM at requested address
+    mcRamReady,       // [Output] RAM is ready to be picked up
+    mcRamAddress,     // [Input] RAM address requested
+    mcRamWrite,       // [Input] RAM to write
+    mcReadReq,        // [Input] RAM read request
+    mcWriteReq,       // [Input] RAM write request
+    mcAddrVirtual,    // [Input] Virtual flag for RAM
+    phRamRead,        // [Input]  RAM at requested address
+    phRamAddress,     // [Output] RAM address requested
+    phRamWrite,       // [Output] RAM to write
+    phReadReq,        // [Output] RAM read request
+    phWriteReq,       // [Output] RAM write request
+    mcDebug           // [Output] Debug port
+  );
+
+  //initial
+     //$monitor("At time %t, ip = %h, opCode = %h, readReq = %h, mcRamReady = %h, ramValue = %h, uartRR = %h, uartRA = %h, uartD = %h, r[0:1:2:3:4:5] = %h:%h:%h:%h:%h:%h, rPos = %h, debug = %h",
+        //$time, iPointer[15:0], opCode, mcReadReq, mcRamReady, mcRamRead, uartReadReq, uartReadAck, uartData, r0, r1, r2, r3, r4, r5, rPos, debug);
+
+endmodule
