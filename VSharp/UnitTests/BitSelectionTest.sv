9530cd054a8d0dcd26736cbfbc0beb98a7627720
Ben Constable <bencon@gmail.com>
2017-08-27 12:44:05 -0700

    Get bit selection test running in simulator

--- VSharp/UnitTests/BitSelectionTest.sv
+++ VSharp/UnitTests/BitSelectionTest.sv
@@ -1,6 +1,5 @@
 module BitSelectionTest(
   reset,
-  in1,
   out1
   );
   // State definitions
@@ -8,17 +7,24 @@ module BitSelectionTest(
   `define __StartState 1
   // inputs / outputs
   input wire reset;
-  input wire[31:0] in1;
   output reg[15:0] out1;
+  reg clk = 0; always #5 clk = !clk;
+  reg[31:0] val;
+  initial
+  begin
+    # 100 $finish;
+  end
   reg [7:0] fsmState = 0;
   always @(posedge clk)
   begin
     case(fsmState)
       `__initial: begin
+        $monitor("val = %h, out1 = %h", val, out1);
+        val <= 291;
         fsmState <= `__StartState;
       end
       `__StartState: begin
-        out1 <= in1[19:4];
+        out1 <= val[19:4];
       end
     endcase
   end
--- VSharp/UnitTests/BitSelectionTest.vs
+++ VSharp/UnitTests/BitSelectionTest.vs
@@ -1,15 +1,26 @@
 module BitSelectionTest(
-	uint<32> in1,
 	out uint<16> out1
 	)
 {
+	clock clk;
+	uint<32> val;
+
+	drive
+	{
+		100: __finish;
+	}
+
 	state initial
 	{
+		__monitor("val = %h, out1 = %h", val, out1);
+
+		val = 0x123;
+
 		transition StartState;
 	}
 
 	state StartState
 	{
-		out1 = in1[19:4];
+		out1 = val[19:4];
 	}
 }
