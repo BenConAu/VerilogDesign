5baa910400ca9b43c9577c6ee8a19f5d6e883c7a
Ben Constable <bencon@gmail.com>
2017-09-27 22:22:01 -0700

    Correct CLZ logic in unit test

--- VSharp/UnitTests/GenericFunctionTest.sv
+++ VSharp/UnitTests/GenericFunctionTest.sv
@@ -29,100 +29,100 @@ module GenericFunctionTest(
                 begin
                   if (HandyNum[0:0])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2 + (32'd2)))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2 + (32'd1)))));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2 + (32'd1)))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2 + (32'd2)))));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[1:1])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd3))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2))));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd3))));
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[3:3] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[2:2])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd1))));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd1))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4 + (32'd2))));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[3:3])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd5)));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd4)));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd5)));
                   end
                 end
               end
             end
             else
             begin
-              if (HandyNum[3:2] == 32'd0)
+              if (HandyNum[7:6] == 32'd0)
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[5:5] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[4:4])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2 + (32'd2))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2 + (32'd1))));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2 + (32'd1))));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2 + (32'd2))));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[5:5])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd3)));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2)));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd3)));
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[7:7] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[6:6])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2)));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd1)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd1)));
+                    CLZ_Handy <= (32'd16 + (32'd8 + (32'd2)));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[7:7])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd9));
+                    CLZ_Handy <= (32'd16 + (32'd8));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd8));
+                    CLZ_Handy <= (32'd16 + (32'd9));
                   end
                 end
               end
@@ -130,108 +130,108 @@ module GenericFunctionTest(
           end
           else
           begin
-            if (HandyNum[7:4] == 32'd0)
+            if (HandyNum[15:12] == 32'd0)
             begin
-              if (HandyNum[3:2] == 32'd0)
+              if (HandyNum[11:10] == 32'd0)
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[9:9] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[8:8])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2 + (32'd2))));
+                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2 + (32'd1))));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2 + (32'd1))));
+                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2 + (32'd2))));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[9:9])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd3)));
+                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2)));
+                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd3)));
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[11:11] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[10:10])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2)));
+                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd1)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd1)));
+                    CLZ_Handy <= (32'd16 + (32'd4 + (32'd2)));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[11:11])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd5));
+                    CLZ_Handy <= (32'd16 + (32'd4));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd4));
+                    CLZ_Handy <= (32'd16 + (32'd5));
                   end
                 end
               end
             end
             else
             begin
-              if (HandyNum[3:2] == 32'd0)
+              if (HandyNum[15:14] == 32'd0)
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[13:13] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[12:12])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd2 + (32'd2)));
+                    CLZ_Handy <= (32'd16 + (32'd2 + (32'd1)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd2 + (32'd1)));
+                    CLZ_Handy <= (32'd16 + (32'd2 + (32'd2)));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[13:13])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd3));
+                    CLZ_Handy <= (32'd16 + (32'd2));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd2));
+                    CLZ_Handy <= (32'd16 + (32'd3));
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[15:15] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[14:14])
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd2));
+                    CLZ_Handy <= (32'd16 + (32'd1));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16 + (32'd1));
+                    CLZ_Handy <= (32'd16 + (32'd2));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[15:15])
                   begin
-                    CLZ_Handy <= (32'd17);
+                    CLZ_Handy <= (32'd16);
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd16);
+                    CLZ_Handy <= (32'd17);
                   end
                 end
               end
@@ -240,110 +240,110 @@ module GenericFunctionTest(
         end
         else
         begin
-          if (HandyNum[15:8] == 32'd0)
+          if (HandyNum[31:24] == 32'd0)
           begin
-            if (HandyNum[7:4] == 32'd0)
+            if (HandyNum[23:20] == 32'd0)
             begin
-              if (HandyNum[3:2] == 32'd0)
+              if (HandyNum[19:18] == 32'd0)
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[17:17] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[16:16])
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2 + (32'd2))));
+                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2 + (32'd1))));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2 + (32'd1))));
+                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2 + (32'd2))));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[17:17])
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd3)));
+                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2)));
+                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd3)));
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[19:19] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[18:18])
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2)));
+                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd1)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd1)));
+                    CLZ_Handy <= (32'd8 + (32'd4 + (32'd2)));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[19:19])
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd5));
+                    CLZ_Handy <= (32'd8 + (32'd4));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd4));
+                    CLZ_Handy <= (32'd8 + (32'd5));
                   end
                 end
               end
             end
             else
             begin
-              if (HandyNum[3:2] == 32'd0)
+              if (HandyNum[23:22] == 32'd0)
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[21:21] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[20:20])
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd2 + (32'd2)));
+                    CLZ_Handy <= (32'd8 + (32'd2 + (32'd1)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd2 + (32'd1)));
+                    CLZ_Handy <= (32'd8 + (32'd2 + (32'd2)));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[21:21])
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd3));
+                    CLZ_Handy <= (32'd8 + (32'd2));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd2));
+                    CLZ_Handy <= (32'd8 + (32'd3));
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[23:23] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[22:22])
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd2));
+                    CLZ_Handy <= (32'd8 + (32'd1));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8 + (32'd1));
+                    CLZ_Handy <= (32'd8 + (32'd2));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[23:23])
                   begin
-                    CLZ_Handy <= (32'd9);
+                    CLZ_Handy <= (32'd8);
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd8);
+                    CLZ_Handy <= (32'd9);
                   end
                 end
               end
@@ -351,114 +351,115 @@ module GenericFunctionTest(
           end
           else
           begin
-            if (HandyNum[7:4] == 32'd0)
+            if (HandyNum[31:28] == 32'd0)
             begin
-              if (HandyNum[3:2] == 32'd0)
+              if (HandyNum[27:26] == 32'd0)
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[25:25] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[24:24])
                   begin
-                    CLZ_Handy <= (32'd4 + (32'd2 + (32'd2)));
+                    CLZ_Handy <= (32'd4 + (32'd2 + (32'd1)));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd4 + (32'd2 + (32'd1)));
+                    CLZ_Handy <= (32'd4 + (32'd2 + (32'd2)));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[25:25])
                   begin
-                    CLZ_Handy <= (32'd4 + (32'd3));
+                    CLZ_Handy <= (32'd4 + (32'd2));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd4 + (32'd2));
+                    CLZ_Handy <= (32'd4 + (32'd3));
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[27:27] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[26:26])
                   begin
-                    CLZ_Handy <= (32'd4 + (32'd2));
+                    CLZ_Handy <= (32'd4 + (32'd1));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd4 + (32'd1));
+                    CLZ_Handy <= (32'd4 + (32'd2));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[27:27])
                   begin
-                    CLZ_Handy <= (32'd5);
+                    CLZ_Handy <= (32'd4);
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd4);
+                    CLZ_Handy <= (32'd5);
                   end
                 end
               end
             end
             else
             begin
-              if (HandyNum[3:2] == 32'd0)
+              if (HandyNum[31:30] == 32'd0)
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[29:29] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[28:28])
                   begin
-                    CLZ_Handy <= (32'd2 + (32'd2));
+                    CLZ_Handy <= (32'd2 + (32'd1));
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd2 + (32'd1));
+                    CLZ_Handy <= (32'd2 + (32'd2));
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[29:29])
                   begin
-                    CLZ_Handy <= (32'd3);
+                    CLZ_Handy <= (32'd2);
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd2);
+                    CLZ_Handy <= (32'd3);
                   end
                 end
               end
               else
               begin
-                if (HandyNum[1:1] == 32'd0)
+                if (HandyNum[31:31] == 32'd0)
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[30:30])
                   begin
-                    CLZ_Handy <= (32'd2);
+                    CLZ_Handy <= (32'd1);
                   end
                   else
                   begin
-                    CLZ_Handy <= (32'd1);
+                    CLZ_Handy <= (32'd2);
                   end
                 end
                 else
                 begin
-                  if (HandyNum[0:0])
+                  if (HandyNum[31:31])
                   begin
-                    CLZ_Handy <= 32'd1;
+                    CLZ_Handy <= 32'd0;
                   end
                   else
                   begin
-                    CLZ_Handy <= 32'd0;
+                    CLZ_Handy <= 32'd1;
                   end
                 end
               end
             end
           end
         end
+        HandyNum <= HandyNum << 32'd3;
       end
     endcase
   end
--- VSharp/UnitTests/GenericFunctionTest.vs
+++ VSharp/UnitTests/GenericFunctionTest.vs
@@ -3,11 +3,11 @@ uint32 CLZ<1>(uint<1> in)
 {
     if (in)
     {
-        return 1;
+        return 0;
     }
     else
     {
-        return 0;
+        return 1;
     }
 }
 
@@ -16,11 +16,13 @@ uint32 CLZ<N>(uint<N> in)
 {
     if (in[N - 1:N/2] == 0)
     {
+        // Top half of input is zero, so count zeros of rest and add
         return (N/2 + CLZ<N/2>(in[N/2 - 1:0]));
     }
     else
     {
-        return CLZ<N/2>(in[N/2 - 1:0]);
+        // Top half is not zero, so count of top half is our value
+        return CLZ<N/2>(in[N-1:N/2]);
     }
 }
 
@@ -41,6 +43,10 @@ module GenericFunctionTest()
 	{
 		__monitor("TestVal = %h", CLZ_Handy);
 
+        // Count leading zeroes
         CLZ_Handy = CLZ<32>(HandyNum);
+
+        // Modify the count
+        HandyNum = HandyNum << 3;
 	}
 }
