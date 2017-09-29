c029f44eaa9e1e7eeffd2fcbb15982554c8949e0
Ben Constable <bencon@gmail.com>
2017-09-29 11:34:49 -0700

    Add code to detect replaceable identifiers for stages

--- VSharp/StatementNode.cpp
+++ VSharp/StatementNode.cpp
@@ -1,7 +1,10 @@
-#include "StatementNode.h"
-#include "ParserContext.h"
 #include "FunctionCallNode.h"
+#include "FunctionDeclaratorNode.h"
 #include "FunctionInfo.h"
+#include "IdentifierNode.h"
+#include "ParserContext.h"
+#include "StatementNode.h"
+#include "VariableInfo.h"
 
 StatementNode::StatementNode(ParserContext* pContext, const YYLTYPE &location) : ASTNode(pContext, location)
 {
@@ -31,33 +34,35 @@ bool StatementNode::PreProcessNodeImpl(OutputContext* pContext)
     if (pFunctionCallNode != nullptr)
     {
         // No declarator means that it is not a function that we defined, so we cannot expand it either
+        // Note that this should not be neccessary based on the GetFirstFunctionCall implementation.
         FunctionDeclaratorNode* pDeclarator = pFunctionCallNode->GetFunctionInfo()->GetFunctionDeclarator();
         if (pDeclarator != nullptr)
         {
             //printf("Found function call %p inside statement %p\n", pFunctionCallNode, this);
-
-            // Check that this statement is the actual owning statement
-            StatementNode* pOwningStatement = pFunctionCallNode->GetTypedParent<StatementNode>();
-            if (pOwningStatement == this)
-            {
-                //printf("Function call %p is owned by statement %p\n", pFunctionCallNode, this);
-                
-                // Create the tree that will replace the expression
-                ASTNode* pReplacement = pFunctionCallNode->ExpandFunction(this);
-        
-                //printf("Created replacement node for function call %p\n", pFunctionCallNode);
-                //pReplacement->DumpNode();
-        
-                // Insert the node after this one
-                GetParent()->InsertChild(GetParent()->GetChildIndex(this) + 1, pReplacement);
-        
-                //printf("Attached replacement node\n");
-                //GetParent()->DumpNode();
-                
-                // Mark this node as replaced so that it won't do anything further
-                return false;
-            }
+            //printf("Function call %p is owned by statement %p\n", pFunctionCallNode, this);
+            
+            // Create the tree that will replace the expression
+            ASTNode* pReplacement = pFunctionCallNode->ExpandFunction(this);
+    
+            //printf("Created replacement node for function call %p\n", pFunctionCallNode);
+            //pReplacement->DumpNode();
+    
+            // Insert the node after this one
+            GetParent()->InsertChild(GetParent()->GetChildIndex(this) + 1, pReplacement);
+    
+            //printf("Attached replacement node\n");
+            //GetParent()->DumpNode();
+            
+            // Mark this node as replaced so that it won't do anything further
+            return false;
         }        
+    }
+
+    // Outputs of stages are basically like function calls, so process them the same way
+    IdentifierNode* pStageOutput = GetFirstStageOutput();
+    if (pStageOutput != nullptr)
+    {
+        printf("Found replaceable identifier %s\n", pStageOutput->GetIdentifierName());
     }
 
     _fProcessed = true;
@@ -86,7 +91,7 @@ bool StatementNode::IsReplaceableFunctionCall(ASTNode* pNode)
         return false;
     }
 
-    // Needs to have function info
+    // Needs to have function info with a declarator
     FunctionInfo* pFunctionInfo = pFunctionCallNode->GetFunctionInfo();
     if (pFunctionInfo == nullptr)
     {
@@ -102,6 +107,39 @@ bool StatementNode::IsReplaceableFunctionCall(ASTNode* pNode)
 
     // Check that this statement is the actual owning statement
     StatementNode* pOwningStatement = pFunctionCallNode->GetTypedParent<StatementNode>();
+    if (pOwningStatement == this)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool StatementNode::IsReplaceableIdentifier(ASTNode* pNode)
+{
+    // Needs to be an identifier
+    IdentifierNode* pIdentifier = dynamic_cast<IdentifierNode*>(pNode);
+    if (pIdentifier == nullptr)
+    {
+        return false;
+    }
+
+    // Needs to have variable info and be an input
+    VariableInfo* pVarInfo = pIdentifier->GetVariableInfo();
+    if (pVarInfo == nullptr || pVarInfo->GetLocationType() != VariableLocationType::Input)
+    {
+        return false;
+    }
+
+    // Check that the owning scope is a stage declaration
+    FunctionDeclaratorNode* pScope = dynamic_cast<FunctionDeclaratorNode*>(pVarInfo->GetScope());
+    if (pScope == nullptr || pScope->GetFunctionType() != FunctionType::Stage)
+    {
+        return false;
+    }
+
+    // Check that this statement is the actual owning statement
+    StatementNode* pOwningStatement = pIdentifier->GetTypedParent<StatementNode>();
     if (pOwningStatement == this)
     {
         return true;
@@ -136,7 +174,38 @@ FunctionCallNode* StatementNode::GetFunctionCall(ASTNode* pNode)
     return nullptr;
 }
 
+IdentifierNode* StatementNode::GetStageOutput(ASTNode* pNode)
+{
+    if (pNode == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (IsReplaceableIdentifier(pNode))
+    {
+        return dynamic_cast<IdentifierNode*>(pNode);
+    }
+
+    for (size_t i = 0; i < pNode->GetChildCount(); i++)
+    {
+        // Do we have a child that is a stage output?
+        IdentifierNode* pStageOutput = GetStageOutput(pNode->GetChild(i));
+        if (pStageOutput != nullptr)
+        {
+            return pStageOutput;
+        }
+    }
+
+    //printf("GetFunctionCall failed on all children and this node is not a function call\n");
+    return nullptr;
+}
+
 FunctionCallNode* StatementNode::GetFirstFunctionCall()
 {
     return GetFunctionCall(this);
+}
+
+IdentifierNode* StatementNode::GetFirstStageOutput()
+{
+    return GetStageOutput(this);
 }
--- VSharp/StatementNode.h
+++ VSharp/StatementNode.h
@@ -4,6 +4,7 @@
 #include "VSharp.tab.h"
 
 class FunctionCallNode;
+class IdentifierNode;
 
 class StatementNode : public ASTNode
 {
@@ -16,11 +17,17 @@ public:
     void SetCallReplacement(FunctionCallNode* pCallNode, ASTNode* pReplacement);
     FunctionCallNode* GetCallNode() { return _pCallNode; }
     ASTNode* GetReplacementNode() { return _pReplacement; }
-    FunctionCallNode* GetFirstFunctionCall();
     
 private:
+    // Function call replacement
+    FunctionCallNode* GetFirstFunctionCall();
     FunctionCallNode* GetFunctionCall(ASTNode* pNode);
     bool IsReplaceableFunctionCall(ASTNode* pNode);
+
+    // Stage output replacement
+    IdentifierNode* GetFirstStageOutput();
+    IdentifierNode* GetStageOutput(ASTNode* pNode);
+    bool IsReplaceableIdentifier(ASTNode* pNode);
     
 private:
     bool _fProcessed;
