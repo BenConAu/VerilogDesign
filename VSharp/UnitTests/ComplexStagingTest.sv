9ee0f2293479dd53331477627510507f9c738963
Ben Constable <bencon@gmail.com>
2017-09-30 15:34:12 -0700

    Add constant expression support for Equal operator

--- VSharp/UnitTests/ComplexStagingTest.sv
+++ VSharp/UnitTests/ComplexStagingTest.sv
@@ -48,35 +48,7 @@ module StagingExample(
     end
     else
     begin
-      if (16'd0 == 16'd1)
-      begin
-        if (moduleInput > 32'd100)
-        begin
-          if (moduleInput > 32'd100)
-          begin
-            moduleOutput <= { 16'd2748, moduleInput[31:16] + moduleInput[15:0] };
-          end
-          else
-          begin
-            moduleOutput <= { 16'd2748, moduleInput[31:16] + 16'd0 };
-          end
-        end
-        else
-        begin
-          if (moduleInput > 32'd100)
-          begin
-            moduleOutput <= { 16'd2748, 16'd0 + moduleInput[15:0] };
-          end
-          else
-          begin
-            moduleOutput <= { 16'd2748, 16'd0 };
-          end
-        end
-      end
-      else
-      begin
-        moduleOutput <= { 16'd2748, 16'd1 };
-      end
+      moduleOutput <= { 16'd2748, 16'd1 };
     end
   end
 endmodule
