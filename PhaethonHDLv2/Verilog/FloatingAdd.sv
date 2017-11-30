09762cde8f1b58781c9c48cab76e2f791b651919
Ben Constable <bencon@gmail.com>
2017-11-29 19:45:16 -0800

    Commit prototype of V# adder

--- PhaethonHDLv2/Verilog/FloatingAdd.sv
+++ PhaethonHDLv2/Verilog/FloatingAdd.sv
@@ -1,14 +1,63 @@
-module FloatingAdd(a, b, negate, out, debug, clk, enable);
-  output reg [31 : 0] out;
-  output reg [31 : 0] debug;
-  input wire clk;
-  input wire [31:0] a;
-  input wire [31:0] b;
-  input wire [0:0] negate;
-  input wire [0:0] enable;
-  integer i;
+import "FloatingHelper.vs";
 
-  `include "FloatingHelper.sv"
+struct PackedFloat
+{
+    bool Sign;
+    uint8 Exponent;
+    uint<23> Mantissa;
+}
+
+struct UnpackedFloat
+{
+    uint32 A_Mantissa;
+    uint8 A_Exponent;
+}
+
+struct UnpackedArguments
+{
+    UnpackedFloat A;
+    UnpackedFloat B;
+}
+
+UnpackedArguments UnpackArguments(PackedFloat a, PackedFloat b, bool negate)
+{
+    // Unpack the mantissa, make b the one with larger exponent
+    // Always negate b if negation is asked for since it is the
+    // second operand.
+    if (a.Exponent < b.Exponent)      
+    {
+        return UnpackedArguments(
+            UnpackFloat(a, false),
+            UnpackFloat(b, negate));
+    }
+    else
+    {
+        return UnpackedArguments(
+            UnpackFloat(b, negate),
+            UnpackFloat(a, false));
+        )
+    }
+}
+
+uint32 CalculateTotalMantissa(UnpackedArguments args, )
+{
+
+}
+
+// Stages for adding numbers
+// Stage 1 - unpack into signed mantissas -> signedMantA, signedMantB
+// Stage 2 - signed shift mantissas to same base -> signedShiftedMantA,  signedShiftedMantB
+// Stage 2 - add mantissas -> totalMant
+// Stage 3 - Set sign bit and de-sign the total mantissa -> signedTotalMant, sign
+// Stage 4 - Adjust mantissa and exponent to correct leading bits -> OutValue
+
+module FloatingAdd(
+    out PackedFloat OutValue,
+    out uint32 debug,
+    PackedFloat a,
+    PackedFloat b,
+    bool negate)
+{
 
   reg        sign;
   reg [7:0]  aExp;
@@ -24,44 +73,26 @@ module FloatingAdd(a, b, negate, out, debug, clk, enable);
   begin
     if (enable == 1)
     begin
-      // Unpack the mantissa, make b the one with larger exponent
-      // Always negate b if negation is asked for since it is the
-      // second operand.
-      if (a[30 : 23] < b[30 : 23])      
-      begin
-        UnpackFloat(a, 0, aMant, aExp);
-        UnpackFloat(b, negate, bMant, bExp);
-      end
-      else
-      begin
-        UnpackFloat(b, negate, aMant, aExp);
-        UnpackFloat(a, 0, bMant, bExp);
-      end
+      UnpackedArguments args = UnpackArguments(a, b, negate);
 
       // Add the mantissas together, shifting the smaller exp one
-      SignedShiftRight(aMant, bExp - aExp, totalMant);
-      totalMant = totalMant + bMant;
+      totalMant = SignedShiftRight(aMant, bExp - aExp) + bMant;
   
       // Figure out sign of total
       if (totalMant[31:31] == 1)
       begin
-        sign = 1;
+        OutValue.Sign = true;
         totalMant = (~totalMant) + 1;
       end
       else
       begin
-        sign = 0;
+        OutValue.Sign = false;
       end
   
-      // Figure out leading zero count
-      CLZ(totalMant, clz);
-  
       // Leading zeros should be 8, so shift to that
-      NormalizeMantissa(totalMant, clz, normMant);
+      OutValue.Mantissa = NormalizeMantissa(totalMant, CLZ(totalMant))[22:0];
   
-      out[31:31] = sign;
-      out[30:23] = bExp + (8 - clz);
-      out[22:0] = normMant[22:0];
+      OutValue.Exponent = bExp + (8 - clz);
     end
     else
     begin
